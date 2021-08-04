9f1fa1378748732e68d9f601d1a50778f044b043
ehuallap <83515313+ehuallap@users.noreply.github.com>
2021-08-04 16:02:08 -0500

    Delete CODE_COVID_RUN/CLASES directory

--- CODE_COVID_RUN/CLASES/escudo.cpp
+++
@@ -1,7 +0,0 @@
-#include "escudo.h"
-escudo::escudo(int _posX) {
-    imgobj = al_load_bitmap("escudo.png");
-    posX = _posX;
-    posY = 476;
-    desplazamiento = 8;
-}
--- CODE_COVID_RUN/CLASES/mascarilla.cpp
+++
@@ -1,7 +0,0 @@
-#include "mascarilla.h"
-mascarilla::mascarilla(int _posX) {
-    imgobj = al_load_bitmap("mascarilla.png");
-    posX = _posX;
-    posY = 482;
-    desplazamiento = 8;
-}
