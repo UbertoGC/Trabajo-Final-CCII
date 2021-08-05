4d07d42471ee50b1b4e44c5fd506390d3642c9fb
ehuallap <83515313+ehuallap@users.noreply.github.com>
2021-08-05 11:43:23 -0500

    Update estructuras.h

--- CODE_COVID_RUN/Clases/estructuras.h
+++ CODE_COVID_RUN/Clases/estructuras.h
@@ -1,29 +1,22 @@
-#ifndef __ESTRUCTURAS_H__
-#define __ESTRUCTURAS_H_
+#ifndef __ESTRUCTURA_H__
+#define __ESTRUCTURA_H__
 
 #include "base_game_CR.h"
 #include "player.h"
 
-
-class estructuras : public base_game_CR {
-private:
+class estructura : public base_game_CR {
+	friend class player;
+protected:
 	int posX;
 	int posY;
 	int velX;
 	ALLEGRO_BITMAP* image;
 	bool solid;
 public:
-	estructuras(int _initposX = 0, int _initposY = 0, int _initVelX = 5, ALLEGRO_BITMAP* _initImage = nullptr, bool _initSolid = false)
+	explicit estructura(int _initposX = 0, int _initposY = 0, int _initVelX = 5, ALLEGRO_BITMAP* _initImage = nullptr, bool _initSolid = false)
 		: posX(_initposX), posY(_initposY), velX(_initVelX), image(_initImage), solid(_initSolid) {}
-	void initDefault() {
-		srand(time(NULL));
-		posX = 1 + rand() % (750);
-		posY = 83 + rand() % (565);
-	}
-	void pinta(int _posX) {
-		//al_draw_bitmap();
-	}
-
+	void initDefault();
+	void pinta();
 };
 
 #endif
