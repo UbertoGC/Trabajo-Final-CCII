36f92607ead3d4b3959805602391ca52cad6ee04
ehuallap <83515313+ehuallap@users.noreply.github.com>
2021-08-05 11:41:52 -0500

    Update escudo.h

--- CODE_COVID_RUN/Clases/escudo.h
+++ CODE_COVID_RUN/Clases/escudo.h
@@ -7,14 +7,15 @@ class escudo : public objeto {
 
 public:
     escudo(int);
-    void efecto(player& _player) {
+    int efecto(player& _player) {
         if (_player.getposX() + 30 >= posX && _player.getposX() <= posX + 40 && _player.getposY() - 4 == posY) {
             _player.cambio(2);
-            uso = true;
+            return 1;
+        }
+        else {
+            return 0;
         }
     }
-    void finalobj(player& _player) {
-        _player.cambio(4);
-    }
+
 };
 #endif
